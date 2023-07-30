eed7e96ac096db86dddf251b0dce375610bf81d4
abas1277 <139161792+abas1277@users.noreply.github.com>
2023-07-30 13:06:37 +0300

    Create task 3_abas.dart

---
+++ task 3_abas.dart
@@ -0,0 +1,21 @@
+void main() {
+  {
+    
+print("-------------------------------------------------3-----------------------------------------------");
+  int number = 7;
+  
+  if (number % 2 == 0) {
+    
+    print("$number is even ");
+    
+  } else {
+    
+    print("$number is odd ");
+    
+  }
+  
+  for (int q = 1; q <= 10; q++) {
+    
+   print(q);
+   }
+  }
