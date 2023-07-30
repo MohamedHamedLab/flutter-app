65f2420e30a88a71cc3374210a1cadf09e8ec02a
abas1277 <139161792+abas1277@users.noreply.github.com>
2023-07-30 13:07:37 +0300

    Create task 5_abas.dart

---
+++ task 5_abas.dart
@@ -0,0 +1,17 @@
+void main() {
+  {
+
+print("-------------------------------------------------5-----------------------------------------------");
+  
+    
+    void greet(String name2) {
+      
+  print(" Hello $name2 How are you ");
+}
+    
+    String theName = "abas";
+    greet(theName);
+    
+   }
+    
+  }
