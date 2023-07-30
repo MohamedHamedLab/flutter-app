f26689b71bafa8d316cc64ee0830d28b33de2c33
abdoahmedibrahim <86499174+abdoahmedibrahim@users.noreply.github.com>
2023-07-30 21:52:14 +0300

    Create Task2_abozaid.dart

---
+++ Task2_abozaid.dart
@@ -0,0 +1,15 @@
+int i = 5;
+double e = 5.5;
+String name = "Abd zaid";
+
+double sum = i+e;
+  print(sum);
+  
+  double sum2 = i-e;
+  print(sum2);
+  
+  double sum3 = i*e;
+  print(sum3);
+  
+  double sum4 = i/e;
+  print(sum4);
