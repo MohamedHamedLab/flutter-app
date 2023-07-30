c0da3992c8017829cb4e36b38b965bbf6d891d47
podegamer <89660397+podegamer@users.noreply.github.com>
2023-07-30 02:33:02 -0700

    Create Task4_Sherif.dart

---
+++ Task4_Sherif.dart
@@ -0,0 +1,15 @@
+void main(){
+  print('// Task 4: Functions');
+  // Task 4: Functions
+  print(sumTwoNumbers(5,5));
+  printGreeting('sheriiiif');
+  
+}
+
+int sumTwoNumbers(int a, int b) {
+  return a + b;
+}
+
+void printGreeting(String name) {
+  print("Hello, $name!");
+}
