7dbeb02bca09279eed94dd517d3042231fea0c20
abas1277 <139161792+abas1277@users.noreply.github.com>
2023-07-30 13:05:32 +0300

    Create Create task 2_abas.dart

---
+++ Create task 2_abas.dart
@@ -0,0 +1,25 @@
+void main() {
+  {
+  
+  print("-------------------------------------------------2-----------------------------------------------");
+  int a = 10;
+    
+  int b = 5;
+  
+  int sum = a + b;
+    
+  int difference  = a - b;
+    
+  int product = a * b;
+    
+  double quotient = a / b;
+  
+  print(" a + b = $sum ");
+    
+  print(" a - b = $difference ");
+    
+  print(" a * b = $product ");
+    
+  print(" a / b = $quotient ");
+  }
+}
