3215363a8f45a20233b9dcf97073542148226400
podegamer <89660397+podegamer@users.noreply.github.com>
2023-07-30 02:30:14 -0700

    Create Task2_Sherif.dart

---
+++ Task2_Sherif.dart
@@ -0,0 +1,14 @@
+// Task 2: Operators
+  print('Task 2: Operators');
+  int num1 = 5;
+  int num2 = 5;
+  
+  int addition = num1 + num2;
+  int subtraction = num1 - num2;
+  int multiplication = num1 * num2;
+  double division = num1 / num2;
+
+  print("Addition: $addition");
+  print("Subtraction: $subtraction");
+  print("Multiplication: $multiplication");
+  print("Division: $division");
