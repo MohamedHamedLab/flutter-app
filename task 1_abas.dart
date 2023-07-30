a57d1c7e60238187e9cc1f056a749b079af0fe5a
abas1277 <139161792+abas1277@users.noreply.github.com>
2023-07-30 13:04:54 +0300

    Create task 1_abas.dart

---
+++ task 1_abas.dart
@@ -0,0 +1,23 @@
+void main() {
+  {
+  
+  print("-----------------------------------------------1-------------------------------------------------");
+    
+  int age = 16;
+    
+  double height = 173.5;
+    
+  String name = "ABAS";
+    
+  bool isMarried = false;
+  
+  print(" Name = $name ");
+    
+  print(" Age = $age ");
+    
+  print(" Height = $height ");
+    
+  print(" Married = $isMarried ");
+
+  }
+}
